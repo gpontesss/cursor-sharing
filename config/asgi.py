041c503f5a827e808e03712cc20ab3309e95ae5f
gpontesss <pontes.guisilva@gmail.com>
2022-03-18 14:06:30 -0300

    Rate position upload to 30/s

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,13 @@
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-from config.urls import wspatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+from config.urls import wspatterns
 
 application = ProtocolTypeRouter(
     {"http": get_asgi_application(), "websocket": wspatterns}
